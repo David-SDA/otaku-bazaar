59ce63b5ae49721cc2993d98088c686b4bec8fbb
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-10-24 16:56:59 +0200

    Suite du header (responsive à régler)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import { faUser, faClipboardList, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faClipboardList, faMagnifyingGlass, faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
     const [categories, setCategories] = useState([]);
+    const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState(false);
+
+    function handleToggleBurgerMenu(){
+        setIsBurgerMenuOpen(!isBurgerMenuOpen)
+    }
 
     function fetchCategoriesData(){
         fetch('http://localhost:5000/categories')
@@ -24,21 +29,70 @@ export default function Header() {
     }, [])
 
     return (
-        <header className='h-24 py-2 border-b border-[#F6BD60] text-neutral-800 font-bold flex items-center'>
+        <header className='h-24 py-2 border-b-2 border-[#F6BD60] text-neutral-800 font-bold flex items-center'>
             <nav className='container mx-auto flex justify-between items-center'>
                 <div>
-                    <NavLink to={`/`} className='bg-orange-500 py-5 px-20 me-7'>Logo</NavLink>
+                    <NavLink to={`/`} className='bg-[#F6BD60] py-5 px-20 ms-4 lg:me-7'>Logo</NavLink>
                     {
                         categories.slice(0, 4).map((category) => (
-                            <NavLink to={`/`} className='me-4' key={category.id}>{category.label}</NavLink>
+                            <NavLink to={`/`} className='hidden lg:inline mx-4 py-1 group relative' key={category.id}>
+                                {category.label}
+                                <span class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-[#F6BD60] group-hover:w-1/2"></span>
+                                <span class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-[#F6BD60] group-hover:w-1/2"></span>
+                            </NavLink>
                         ))
                     }
-                    <NavLink to={`/search`} className='ms-4'>More</NavLink>
+                    <NavLink to={`/search`} className='hidden lg:inline mx-4 py-1 group relative'>
+                        More
+                        <span class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-[#F6BD60] group-hover:w-1/2"></span>
+                        <span class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-[#F6BD60] group-hover:w-1/2"></span>
+                    </NavLink>
                 </div>
-                <div>
-                    <NavLink to={`/search`}><FontAwesomeIcon icon={faMagnifyingGlass} size='lg' className='me-4' /></NavLink>
-                    <NavLink to={`/wishlist`}><FontAwesomeIcon icon={faClipboardList} size='lg' className='mx-4' /></NavLink>
-                    <NavLink to={`/login`}><FontAwesomeIcon icon={faUser} size='lg' className='ms-4' /></NavLink>
+                <div className='hidden lg:block'>
+                    <NavLink to={`/search`} className='group relative me-4 py-1'>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} size='lg' />
+                        <span class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-[#F6BD60] group-hover:w-1/2"></span>
+                        <span class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-[#F6BD60] group-hover:w-1/2"></span>
+                    </NavLink>
+                    <NavLink to={`/wishlist`} className='group relative mx-4 py-1'>
+                        <FontAwesomeIcon icon={faClipboardList} size='lg'/>
+                        <span class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-[#F6BD60] group-hover:w-1/2"></span>
+                        <span class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-[#F6BD60] group-hover:w-1/2"></span>
+                    </NavLink>
+                    <NavLink to={`/login`} className='group relative mx-4 py-1'>
+                        <FontAwesomeIcon icon={faUser} size='lg' />
+                        <span class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-[#F6BD60] group-hover:w-1/2"></span>
+                        <span class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-[#F6BD60] group-hover:w-1/2"></span>
+                    </NavLink>
+                </div>
+                <div className='block bg-[#F6BD60] mx-3 py-2 px-3 rounded-md lg:hidden' onClick={handleToggleBurgerMenu}>
+                    <FontAwesomeIcon icon={faBars} size='2xl' />
+                </div>
+                <div className={`lg:hidden absolute top-0 right-0 h-screen w-80 py-5 bg-[#F6BD60] flex flex-col transition-transform duration-300 ${isBurgerMenuOpen ? 'translate-x-0' : 'translate-x-80'}`}>
+                    <FontAwesomeIcon icon={faXmark} size='2xl' className='mx-3 py-2 px-3 rounded-md bg-[#F4F6EE] self-end' onClick={handleToggleBurgerMenu} />
+                    {
+                        categories.slice(0, 4).map((category) => (
+                            <NavLink to={`/`} className='w-full py-3 text-center' key={category.id} onClick={handleToggleBurgerMenu}>
+                                {category.label}
+                            </NavLink>
+                        ))
+                    }
+                    <NavLink to={`/search`} className='w-full py-3 text-center' onClick={handleToggleBurgerMenu}>More</NavLink>
+                    <div className='h-px w-11/12 bg-[#F4F6EE] mx-auto my-3'></div>
+                    <div className='flex flex-col items-center'>
+                        <NavLink to={`/search`} className='w-full py-3 text-center' onClick={handleToggleBurgerMenu}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} size='lg' className='me-3' />
+                            Search
+                        </NavLink>
+                        <NavLink to={`/wishlist`} className='w-full py-3 text-center' onClick={handleToggleBurgerMenu}>
+                            <FontAwesomeIcon icon={faClipboardList} size='lg' className='me-3' />
+                            Wishlist
+                        </NavLink>
+                        <NavLink to={`/login`} className='w-full py-3 text-center' onClick={handleToggleBurgerMenu}>
+                            <FontAwesomeIcon icon={faUser} size='lg' className='me-3' />
+                            Login
+                        </NavLink>
+                    </div>
                 </div>
             </nav>
         </header>
