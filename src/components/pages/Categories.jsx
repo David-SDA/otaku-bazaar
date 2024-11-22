8b22399e69e6f640c5bed27005c69a62ba3ea6da
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-11-22 16:56:57 +0100

    Ajout de la page catégorie et ses composants

--- src/components/pages/Categories.jsx
+++ src/components/pages/Categories.jsx
@@ -4,7 +4,7 @@ import CategoryCard from '../ui/CategoryCard';
 
 export default function Categories(){
     return (
-        <div className='container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        <div className='container mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
             {
                 data.categories.map((category) => (
                     <CategoryCard
--- src/components/ui/CategoryCard.jsx
+++ src/components/ui/CategoryCard.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function CategoryCard({category, categoryImage}){
     return (
-        <div className='rounded-lg min-h-32 relative border border-red-500'>
-            <img src={categoryImage} alt="" className='absolute top-0 left-0 object-cover'/>
-            <p>{category}</p>
-        </div>
+        <Link className='rounded-lg h-32 relative shadow overflow-hidden group hover:scale-105 hover:border hover:border-[#F5CAC3] transition-all duration-300 ease-out select-none'>
+            <img src={categoryImage} alt="" className='w-full h-full object-cover'/>
+            <div className='w-full h-full absolute top-0 left-0 bg-black opacity-15 group-hover:bg-[#F7EDE2] group-hover:opacity-100 transition-all duration-300 ease-out'></div>
+            <p className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-10 font-bold sm:text-lg group-hover:text-primary transition-all duration-300 ease-out'>
+                {category}
+            </p>
+        </Link>
     )
 }
