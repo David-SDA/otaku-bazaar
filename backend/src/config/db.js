7714d2f0034c23c87e651d939d3b52ba40adb20d
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-12-20 15:27:18 +0100

    Reset du back pour migrer vers postgre

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,15 +1,3 @@
-import mongoose from 'mongoose';
-
 export async function connectToDatabase(){
-    try{
-        const dbUri = process.env.DB_URI;
-        const options = { authSource: process.env.DB_AUTH_SOURCE };
-
-        await mongoose.connect(dbUri, options);
-        console.log('Connected successfully to the database');
-    }
-    catch(error){
-        console.error(`Error connecting to the database : ${error}`);
-        throw error;
-    }
+    
 }
