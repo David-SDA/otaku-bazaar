e0f8decd95c2efadb52780b2f96d945f06c4ac62
David Sousa de Araujo <davidaraujo2001@gmail.com>
2025-01-21 22:15:17 +0100

    feat : ajout appel api pour la page moderateur

--- frontend/src/components/pages/ModeratorPanel.jsx
+++ frontend/src/components/pages/ModeratorPanel.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AnnouncementsReportedTable from '../ui/announcements/AnnouncementsReportedTable';
+import LoadingAnimation from '../ui/general/LoadingAnimation';
 
 export default function ModeratorPanel(){
+    const [reportedAnnouncements, setReportedAnnouncements] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchModeratorPanel(){
+            try{
+                const response = await fetch('http://localhost:8000/announcements/reported', {credentials: 'include'});
+                if(!response.ok){
+                    throw new Error('Failed to fetch reported announcements');
+                }
+                const data = await response.json();
+                setReportedAnnouncements(data);
+            }
+            catch(error){
+                console.error('Error fetching moderator panel:', error);
+            }
+            finally{
+                setLoading(false);
+            }
+        }
+        
+        fetchModeratorPanel();
+    }, [])
+
     return (
         <>
-            <h1 className='text-center text-xl lg:text-2xl font-bold mb-5'>Moderator Panel</h1>
-            <h2 className='text-xl font-bold mb-3'>Reported Announcements</h2>
-            <AnnouncementsReportedTable />
+            {
+                isLoading ? (
+                    <LoadingAnimation />
+                ) : (
+                    <>
+                        <h1 className='text-center text-xl lg:text-2xl font-bold mb-5'>Moderator Panel</h1>
+                        <h2 className='text-xl font-bold mb-3'>Reported Announcements</h2>
+                        <AnnouncementsReportedTable reportedAnnouncements={reportedAnnouncements} />
+                    </>
+                )
+            }
         </>
     );
 }
