51afdabc8c9e4cdf850f06b8597886df6466cb19
David Sousa de Araujo <davidaraujo2001@gmail.com>
2025-06-09 00:33:55 +0200

    Correction dotenv

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,38 +1,7 @@
-// import express from 'express';
-// import cors from 'cors';
-// import dotenv from 'dotenv';
-// import cookieParser from 'cookie-parser';
-// import categoriesRoutes from './routes/categoriesRoutes.js';
-// import announcementsRoutes from './routes/announcementsRoutes.js';
-// import imagesRoutes from './routes/imagesRoutes.js';
-// import usersRoutes from './routes/usersRoutes.js';
-// import authRoutes from './routes/authRoutes.js';
-
-// dotenv.config();
-
-// const app = express();
-// app.use(express.json());
-// app.use(cors(
-//     {
-//         origin: `${process.env.FRONTEND_URL}`,
-//         credentials: true,
-//     }
-// ));
-// app.use(cookieParser());
-
-// app.use('/categories', categoriesRoutes);
-// app.use('/announcements', announcementsRoutes);
-// app.use('/images', imagesRoutes);
-// app.use('/users', usersRoutes);
-// app.use('/auth', authRoutes);
-// app.use('/uploads', express.static('uploads'));
-
-// const PORT = process.env.PORT || 8000;
-// app.listen(PORT, () => {
-//     console.log('Server is running on port' + PORT);
-// });
-
+import 'dotenv/config';
 import app from './app.js';
+
+dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
