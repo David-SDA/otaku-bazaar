9807d3954fa08064592f3904b99a72ac1504c6a5
David Sousa de Araujo <davidaraujo2001@gmail.com>
2025-06-09 01:06:13 +0200

    ajout d'un test.yml

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,7 +6,8 @@ dotenv.config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' });
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    logging: false,
 })
 
 sequelize.authenticate()
